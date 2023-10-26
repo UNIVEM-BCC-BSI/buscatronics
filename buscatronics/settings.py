f2cff52acab4a41dd62a70b51e95361604c3c722
Arthur <arthur.hoshino@gmail.com>
2023-10-26 00:53:46 -0300

    env exemplo, pycache removido

--- buscatronics/settings.py
+++ buscatronics/settings.py
@@ -10,20 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#1zp70t7pf@o7s!rnb_30cay%kf^9xh1$-!!-%-1gkuxo#sls@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
